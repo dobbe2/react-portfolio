cb2e9637ecc77872b0ac4d6add78797663b0f72f
James Dobbe <dobbe2@gmail.com>
2020-06-04 16:12:20 -0700

    Updates

---
+++ precache-manifest.1b8e565903d19f9e08f9f2dd08a93edd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce3af1a8f5d602ca50bd5d4a4d7aedb",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "006eb421783d914920c2",
+    "url": "/react-portfolio/static/css/main.b4372cff.chunk.css"
+  },
+  {
+    "revision": "c0668f8d642285bd7c5a",
+    "url": "/react-portfolio/static/js/2.5a1e88c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.5a1e88c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006eb421783d914920c2",
+    "url": "/react-portfolio/static/js/main.f47143d9.chunk.js"
+  },
+  {
+    "revision": "0938b211af4f266a20b0",
+    "url": "/react-portfolio/static/js/runtime-main.0319e79d.js"
+  },
+  {
+    "revision": "11fabfc60a2b9332300a73e087f25200",
+    "url": "/react-portfolio/static/media/chill-background.11fabfc6.jpg"
+  },
+  {
+    "revision": "3cfefa35369a9118a0cd5a19f61686ff",
+    "url": "/react-portfolio/static/media/family-portrait.3cfefa35.jpeg"
+  },
+  {
+    "revision": "3fbb6d4dc500954979e9b27bc4b117b7",
+    "url": "/react-portfolio/static/media/git-hub-2.3fbb6d4d.jpg"
+  },
+  {
+    "revision": "f9adcf3133c6577e114636d9ce08a33d",
+    "url": "/react-portfolio/static/media/linkedin.f9adcf31.jpg"
+  },
+  {
+    "revision": "ba48ea8a4510b4ec601fc57b9601a251",
+    "url": "/react-portfolio/static/media/male3.ba48ea8a.jpg"
+  },
+  {
+    "revision": "62d65c990121c37c57ad6cbb969662b0",
+    "url": "/react-portfolio/static/media/pacman.62d65c99.jpg"
+  },
+  {
+    "revision": "f387b29a4a6f52135d44566b9672c185",
+    "url": "/react-portfolio/static/media/portfolioPic.f387b29a.jpg"
+  }
+]);
